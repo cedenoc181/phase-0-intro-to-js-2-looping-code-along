8191dfbd4f18cf28cc624253973ceac55adf9738
Christian <cedenoc181@gmail.com>
2022-09-05 15:13:52 -0400

    commit tree b394bb77cba79951c0fadefe1ca3781517c83eb3

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+len = names.length;
+function writeCards(names){
+for (let c = 0;c < len; c++){
+    console.log(`thank you ${names[c]} for the bitch ass gift`);
+    debugger;
+}
+}
+writeCards(names);
+
+
+let countdown = 10;
+while (countdown > -1 ){
+    console.log(countdown--);
+
+}
